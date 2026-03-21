3574d13d7cf52a8f1f59cabde338ad55e5ada7bb
brunobarmac <brunobarmac.0@gmail.com>
2026-03-20 22:21:35 -0300

    aula04

--- Aula01/routes/web.php
+++ Aula01/routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+Route::get('/', function() {
+    return view ('welcome');
 });
 
-require __DIR__.'/settings.php';
+Route::get('/about', function() {
+    return view ('TeleSobre');
+});
